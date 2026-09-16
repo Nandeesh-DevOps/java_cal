5e59255e2e32f3faf3419f88d16f50cdd5d967aa
Nandeesh-DevOps <nandeesh.nnbb143@gmail.com>
2026-09-16 19:58:15 +0530

    Update CalculatorApplication.java

--- src/main/java/com/calculator/CalculatorApplication.java
+++ src/main/java/com/calculator/CalculatorApplication.java
@@ -1,19 +1,20 @@
-```java
 package com.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CalculatorApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
 
-        SpringApplication.run(
-                CalculatorApplication.class,
-                args
-        );
-
+    @GetMapping("/")
+    public String home() {
+        return "Java Calculator is running!";
     }
 }
-```
